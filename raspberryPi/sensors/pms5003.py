974ad54cd042f8a47d0653704b864578584df7bd
gseaba <gseaba@uiowa.edu>
2026-05-03 17:33:32 -0500

    Update pms5003.py

--- raspberryPi/sensors/pms5003.py
+++ raspberryPi/sensors/pms5003.py
@@ -5,15 +5,9 @@ class PMS5003Sensor:
     def __init__(self, port="/dev/serial0", baudrate=9600):
         uart = serial.Serial(port, baudrate, timeout=1.5)
         self.sensor = PM25_UART(uart, reset_pin=None)
-        self.sensor.active_mode()
 
     def read(self):
         try:
-            # Clear the old buffer data
-            self.sensor.uart.reset_input_buffer()
-
-            time.sleep(0.5)
-            
             data = self.sensor.read()
             if not data:
                 return None
